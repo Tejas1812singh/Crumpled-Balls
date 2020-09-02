b10aac83720d116034498a3535e3bcd606a71171
Tejas1812singh <67689247+Tejas1812singh@users.noreply.github.com>
2020-09-02 14:04:21 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,35 @@
+class Paper{
+    constructor(x,y,radius)
+    {
+    
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:0.5
+    
+           
+        }
+    this.bodies=Matter.Bodies.circle(x, y, radius, options)
+    this.radius=radius
+    World.add(world,this.bodies)
+    }
+    display()
+    {
+        var pos =this.bodies.position;
+        var angle = this.bodies.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        ellipseMode(RADIUS);
+        strokeWeight(4);
+        
+        fill(255);
+        ellipse( 0, 0,this.radius, this.radius);
+        pop();
+      
+
+     
+    }
+}
+
